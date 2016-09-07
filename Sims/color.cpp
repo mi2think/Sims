0e727f9640521d548c1d1b71894c56ba29c32cca
Wenbiao Hou <mi2think@gmail.com>
2016-09-07 21:24:57 +0800

    Add Color

---
+++ Sims/color.cpp
@@ -0,0 +1,22 @@
+/********************************************************************
+	created:	2016/09/07
+	created:	7:9:2016   21:18
+	filename: 	D:\Code\Sims\Sims\color.cpp
+	file path:	D:\Code\Sims\Sims
+	file base:	color
+	file ext:	cpp
+	author:		mi2think@gmail.com
+
+	purpose:	Color
+*********************************************************************/
+#include "color.h"
+
+namespace sims
+{
+	const Color Color::white = Color(255, 255, 255);
+	const Color Color::gray = Color(100, 100, 100);
+	const Color Color::black = Color(0, 0, 0);
+	const Color Color::red = Color(255, 0, 0);
+	const Color Color::green = Color(0, 255, 0);
+	const Color Color::blue = Color(0, 0, 255);
+}
---
+++ Sims/color.h
@@ -0,0 +1,81 @@
+/********************************************************************
+	created:	2016/09/07
+	created:	7:9:2016   20:02
+	filename: 	D:\Code\Sims\Sims\color.h
+	file path:	D:\Code\Sims\Sims
+	file base:	color
+	file ext:	h
+	author:		mi2think@gmail.com
+
+	purpose:	Color
+*********************************************************************/
+#ifndef __COLOR_H__
+#define __COLOR_H__
+
+#include "sims.h"
+
+namespace sims
+{
+	class Color
+	{
+	public:
+		union
+		{
+			struct 
+			{
+				uint8 b;
+				uint8 g;
+				uint8 r;
+				uint8 a;
+			};
+			uint32 value;
+		};
+		Color() {}
+		Color(uint8 _r, uint8 _g, uint8 _b, uint8 _a = 255)
+			: r(_r)
+			, g(_g)
+			, b(_b)
+			, a(_a)
+		{}
+		Color(uint32 val) : value(val) {}
+
+		Color& operator*=(float k)
+		{
+			r = (uint8)clamp_t(r * k, 0.0f, 255.0f);
+			g = (uint8)clamp_t(g * k, 0.0f, 255.0f);
+			b = (uint8)clamp_t(b * k, 0.0f, 255.0f);
+			a = (uint8)clamp_t(a * k, 0.0f, 255.0f);
+			return *this;
+		}
+
+		Color& operator+=(const Color& c)
+		{
+			r = MIN(r + c.r, 255);
+			g = MIN(g + c.g, 255);
+			b = MIN(b + c.b, 255);
+			a = MIN(a + c.a, 255);
+			return *this;
+		}
+
+		static const Color white;
+		static const Color gray;
+		static const Color black;
+		static const Color red;
+		static const Color green;
+		static const Color blue;
+	};
+
+	inline Color operator*(const Color& c, float k)
+	{
+		Color color = c;
+		return color *= k;
+	}
+
+	inline Color operator*(float k, const Color& c)
+	{
+		Color color = c;
+		return color *= k;
+	}
+}
+
+#endif
