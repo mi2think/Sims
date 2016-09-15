7aad848946b3edc1bbfe9c62395eddfc39f2def7
Wenbiao Hou <mi2think@gmail.com>
2016-09-15 15:37:24 +0800

    Add fps

---
+++ Sims/fps.h
@@ -0,0 +1,43 @@
+/********************************************************************
+	created:	2016/09/15
+	created:	15:9:2016   15:30
+	filename: 	D:\Code\Sims\Sims\fps.h
+	file path:	D:\Code\Sims\Sims
+	file base:	fps
+	file ext:	h
+	author:		mi2think@gmail.com
+	
+	purpose:	FPS
+*********************************************************************/
+#ifndef __FPS_H__
+#define __FPS_H__
+
+#include "sims.h"
+
+namespace sims
+{
+	class Fps
+	{
+	public:
+		Fps() : timeElapsed_(0.0), frameCount_(0), fps_(0.0f) {}
+
+		void OnUpdate(const Timestep& timestep)
+		{
+			++frameCount_;
+			timeElapsed_ += timestep.GetSeconds();
+			if (timeElapsed_ >= 0.5f)
+			{
+				fps_ = frameCount_ / timeElapsed_;
+				frameCount_ = 0;
+				timeElapsed_ = 0.0f;
+			}
+		}
+		float Count() const { return fps_; }
+	private:
+		float timeElapsed_;
+		uint32 frameCount_;
+		float fps_;
+	};
+}
+
+#endif
