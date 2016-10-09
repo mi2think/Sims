38cd52487cf09534b180af85616c7af1f33d12de
Wenbiao Hou <mi2think@gmail.com>
2016-10-10 01:24:31 +0800

    Save global d3d11 var to Window for easy access

--- Sims/graphics_api/dx11/common_dx11.h
+++ Sims/graphics_api/dx11/common_dx11.h
@@ -12,8 +12,13 @@
 #ifndef __COMMON_DX11_H__
 #define __COMMON_DX11_H__
 
+#pragma warning(push)
+#pragma warning( disable : 4838 )  // disable narrow-cast warning in xnamath
+
 #include "sims.h"
 
+#include <windows.h> // must forward include if include xnamath
+#include <xnamath.h>
 #include <dxgi.h>
 #include <d3d11.h>
 #include <d3dx11.h>
@@ -22,6 +27,17 @@ namespace sims
 {
 	namespace dx11
 	{
+		XMGLOBALCONST XMVECTORF32 White   = { 1.0f, 1.0f, 1.0f, 1.0f };
+		XMGLOBALCONST XMVECTORF32 Black   = { 0.0f, 0.0f, 0.0f, 1.0f };
+		XMGLOBALCONST XMVECTORF32 Red     = { 1.0f, 0.0f, 0.0f, 1.0f };
+		XMGLOBALCONST XMVECTORF32 Green   = { 0.0f, 1.0f, 0.0f, 1.0f };
+		XMGLOBALCONST XMVECTORF32 Blue    = { 0.0f, 0.0f, 1.0f, 1.0f };
+		XMGLOBALCONST XMVECTORF32 Yellow  = { 1.0f, 1.0f, 0.0f, 1.0f };
+		XMGLOBALCONST XMVECTORF32 Cyan    = { 0.0f, 1.0f, 1.0f, 1.0f };
+		XMGLOBALCONST XMVECTORF32 Magenta = { 1.0f, 0.0f, 1.0f, 1.0f };
+		XMGLOBALCONST XMVECTORF32 Silver  = { 0.75f, 0.75f, 0.75f, 1.0f };
+		XMGLOBALCONST XMVECTORF32 LightSteelBlue = { 0.69f, 0.77f, 0.87f, 1.0f };
+
 		// error checker
 		class DXErrorChecker
 		{
@@ -51,5 +67,5 @@ namespace sims
 	}
 }
 
-
+#pragma warning( pop )  
 #endif
--- Sims/graphics_api/dx11/window_dx11.h
+++ Sims/graphics_api/dx11/window_dx11.h
@@ -23,15 +23,45 @@ namespace sims
 		class Window : public WindowBase
 		{
 		public:
+			Window()
+				: pD3DD_(nullptr)
+				, pD3DDC_(nullptr)
+				, pSwapChain_(nullptr)
+				, pDepthStencilBuffer_(nullptr)
+				, pRenderTargetView_(nullptr)
+				, pDepthStencilView_(nullptr)
+			{}
+
 			virtual void OnCreate()
 			{
 				SetupD3DDevice(GetHandle(), width_, height_, wndmode_);
+
+				pD3DD_ = g_pD3DD;
+				pD3DDC_ = g_pD3DDC;
+				pSwapChain_ = g_pSwapChain;
+				pDepthStencilBuffer_ = g_pDepthStencilBuffer;
+				pRenderTargetView_ = g_pRenderTargetView;
+				pDepthStencilView_ = g_pDepthStencilView;
 			}
 
 			virtual void OnDestroy()
 			{
 				DestoryDirectX();
+
+				pD3DD_ = nullptr;
+				pD3DDC_ = nullptr;
+				pSwapChain_ = nullptr;
+				pDepthStencilBuffer_ = nullptr;
+				pRenderTargetView_ = nullptr;
+				pDepthStencilView_ = nullptr;
 			}
+		protected:
+			ID3D11Device* pD3DD_;
+			ID3D11DeviceContext* pD3DDC_;
+			IDXGISwapChain* pSwapChain_;
+			ID3D11Texture2D* pDepthStencilBuffer_;
+			ID3D11RenderTargetView* pRenderTargetView_;
+			ID3D11DepthStencilView* pDepthStencilView_;
 		};
 	}
 }
