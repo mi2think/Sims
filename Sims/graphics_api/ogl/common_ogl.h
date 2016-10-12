fa1026a297bbc9f18272d3df99fae7ce82620bc7
Wenbiao Hou <mi2think@gmail.com>
2016-10-12 19:18:46 +0800

    Add common ogl

---
+++ Sims/graphics_api/ogl/common_ogl.h
@@ -0,0 +1,32 @@
+/********************************************************************
+	created:	2016/10/12
+	created:	12:10:2016   16:56
+	filename: 	D:\Code\Sims\Sims\graphics_api\ogl\common_ogl.h
+	file path:	D:\Code\Sims\Sims\graphics_api\ogl
+	file base:	common_ogl
+	file ext:	h
+	author:		mi2think@gmail.com
+	
+	purpose:	OGL
+*********************************************************************/
+#ifndef __COMMON_OGL_H__
+#define __COMMON_OGL_H__
+
+#define FREEGLUT_STATIC // defined so you can link to freeglut_static.lib when compiling
+#define GLEW_STATIC     // defined so you can link to glew's static .lib when compiling
+
+#include "sims.h"
+
+#include <glew.h>
+#include <freeglut.h>
+
+namespace sims
+{
+	namespace ogl
+	{
+
+	}
+}
+
+
+#endif
