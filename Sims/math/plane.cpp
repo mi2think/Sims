250089689b162ff73f2de59f741b32b9b900cc46
Wenbiao Hou <mi2think@gmail.com>
2016-11-13 22:53:19 +0800

    Add plane

---
+++ Sims/math/plane.cpp
@@ -0,0 +1,75 @@
+/********************************************************************
+	created:	2016/11/13
+	created:	13:11:2016   22:41
+	filename: 	D:\Code\Sims\Sims\math\plane.cpp
+	file path:	D:\Code\Sims\Sims\math
+	file base:	plane
+	file ext:	cpp
+	author:		mi2think@gmail.com
+
+	purpose:	Plane
+*********************************************************************/
+#include "plane.h"
+#include "matrix44.h"
+
+namespace sims
+{
+	Plane::Plane(const Vector3f& n, float d)
+		: n_(n)
+		, d_(d)
+	{
+		assert(n.IsNormalized());
+	}
+
+	Plane::Plane(const Vector3f& p0, const Vector3f& p1, const Vector3f& p2)
+	{
+		Vector3f u = p1 - p0;
+		Vector3f v = p2 - p0;
+		n_ = CrossProduct(u, v);
+		n_.Normalize();
+		d_ = -DotProduct(n_, p0);
+	}
+
+	Plane::Plane(const Vector3f& n, const Vector3f& p)
+		: n_(n)
+	{
+		assert(n_.IsNormalized());
+		d_ = -DotProduct(n_, p);
+	}
+
+	Plane::Plane(float a, float b, float c, float d)
+		: n_(a, b, c)
+		, d_(d)
+	{
+		Normalize();
+	}
+
+	float Plane::Distance(const Vector3f& p) const
+	{
+		return DotProduct(n_, p) + d_;
+	}
+
+	Vector3f Plane::NearestPoint(const Vector3f& p) const
+	{
+		// we assume q is the nearest point in plane for p
+		// and k is the nearest distance from p to plane. since n is unit-vector
+		// such: q = p + (-kn), and k = Distance(p)
+		return p - Distance(p) * n_;
+	}
+
+	void Plane::Normalize()
+	{
+		float len = n_.Length();
+		float f = 1.0f / len;
+		n_ *= f;
+		d_ *= f; // we also need divide d by len
+	}
+
+	Plane TransformPlane(const Plane& plane, const Matrix44f& m)
+	{
+		// transform plane as vector4, see formula in page 102
+		Vector4f v(plane.n_.x, plane.n_.y, plane.n_.z, plane.d_);
+		v *= m;
+		return Plane(Vector3f(v.x, v.y, v.z), v.w);
+	}
+}
---
+++ Sims/math/plane.h
@@ -0,0 +1,51 @@
+/********************************************************************
+	created:	2016/11/13
+	created:	13:11:2016   22:32
+	filename: 	D:\Code\Sims\Sims\math\plane.h
+	file path:	D:\Code\Sims\Sims\math
+	file base:	plane
+	file ext:	h
+	author:		mi2think@gmail.com
+
+	purpose:	Plane
+*********************************************************************/
+#ifndef __PLANE_H__
+#define __PLANE_H__
+
+#include "math_fwd.h"
+#include "vector3.h"
+
+namespace sims
+{
+	// for a plane, is a collection of p, which n <dot> (p - p0) = 0, then n <dot> p - n <dot> p0 = 0
+	// we know: x * n.x + y * n.y + z * n.z + d = 0
+	// so: d = -n.p0, then: n.p + d = 0
+	class Plane
+	{
+	public:
+		Vector3f n_; // normal
+		float d_;	 // d
+
+		Plane() = default;
+		Plane(const Vector3f& n, float d);
+		Plane(const Vector3f& n, const Vector3f& p);
+		Plane(const Vector3f& p0, const Vector3f& p1, const Vector3f& p2);
+		Plane(float a, float b, float c, float d);
+
+		// signed distance:
+		// =0: p lies in plane
+		// <0: p lies on negative side of plane
+		// >0: p lies on positive side of plane
+		float Distance(const Vector3f& p) const;
+
+		// nearest point in plane
+		Vector3f NearestPoint(const Vector3f& p) const;
+
+		void Normalize();
+	};
+
+	// transform plane by matrix
+	Plane TransformPlane(const Plane& plane, const Matrix44f& m);
+}
+
+#endif
