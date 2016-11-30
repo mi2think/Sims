3aeeed3c93418b55a08ddfcd626f4fffdd1bee7e
Wenbiao Hou <mi2think@gmail.com>
2016-11-30 16:03:26 +0800

    Complete vertex buffer

--- Sims/graphics/vertex_buffer.cpp
+++ Sims/graphics/vertex_buffer.cpp
@@ -13,8 +13,120 @@
 
 namespace sims
 {
+	LockedVertexBuffer::LockedVertexBuffer()
+	{
+		Clear();
+	}
+
+	LockedVertexBuffer::LockedVertexBuffer(VertexBuffer* vertexBuffer, uint32 lockFlags, uint32 offset, uint32 count)
+	{
+		Init(vertexBuffer, lockFlags, offset, count);
+	}
+
+	const VertexDeclarationRef& LockedVertexBuffer::GetVertexDeclaration() const
+	{
+		return vertexBuffer_->GetVertexDeclaration();
+	}
+
+	void* LockedVertexBuffer::GetData()
+	{
+		return vertexData_->GetData();
+	}
+
+	const void* LockedVertexBuffer::GetData() const
+	{
+		return vertexData_->GetData();
+	}
+
+	void LockedVertexBuffer::Clear()
+	{
+		vertexBuffer_ = nullptr;
+		offset_ = count_ = 0;
+		lockFlags_ = VertexBuffer::LockRead;
+		vertexData_ = nullptr;
+	}
+
+	void LockedVertexBuffer::Init(VertexBuffer* vertexBuffer, uint32 lockFlags, uint32 offset, uint32 count)
+	{
+		vertexBuffer_ = vertexBuffer;
+		offset_ = offset;
+		count_ = count;
+		lockFlags_ = lockFlags;
+		vertexData_ = vertexBuffer_->GetVertexData();
+	}
+
+	VertexBuffer::VertexBuffer()
+		: vertexCount_(0)
+		, storageFlags_(StorageFlags::Local)
+		, lockedCount_(0)
+	{}
+
 	VertexBuffer::VertexBuffer(const VertexDeclarationRef& vertexDecl, uint32 vertexCount)
+		: vertexDecl_(vertexDecl)
+		, vertexCount_(vertexCount)
+		, storageFlags_(StorageFlags::Local)
+		, lockedCount_(0)
+	{
+		vertexData_.Resize(vertexCount_ * vertexDecl->GetStride());
+	}
+
+	VertexBuffer::~VertexBuffer()
 	{
 
+	}
+
+	LockedVertexBuffer* VertexBuffer::Lock(uint32 lockFlags, uint32 offset, uint32 count)
+	{
+		if (lockedCount_ > 0)
+		{
+			// read is ok if pre-lock is read
+			if ((lockFlags & LockWrite) != 0 ||
+				(lockedVB_.lockFlags_ & LockWrite) != 0)
+			{
+				ASSERT(false && "lock locked image");
+				return nullptr;
+			}
+		}
+
+		if (!Valid())
+		{
+			ASSERT(false && "vertex buffer has no data");
+			return nullptr;
+		}
+		if (count == 0)
+			count = vertexCount_ - offset;
+		if (offset + count > vertexCount_)
+		{
+			ASSERT(false && "over range of vertex buffer");
+			return nullptr;
+		}
+
+		++lockedCount_;
+		lockedVB_.Init(this, lockFlags, offset, count);
+		return &lockedVB_;
+	}
+
+	void VertexBuffer::Unlock(LockedVertexBuffer* L)
+	{
+		if (!L || L->vertexBuffer_ != this)
+		{
+			ASSERT(false && "can not unlock vertex buffer");
+			return;
+		}
+
+		--lockedCount_;
+		if (lockedCount_ == 0)
+			L->Clear();
+		else
+			ASSERT(L->GetLockFlags() == LockRead && "multi-lock must be read");
+	}
+
+	void VertexBuffer::Invalidate()
+	{
+		if ((storageFlags_ & StorageFlags::Hardware) == 0)
+			return;
+
+		// update vertex buffer
+		HWUpdateVertexBuffer();
 	}
 }
--- Sims/graphics/vertex_buffer.h
+++ Sims/graphics/vertex_buffer.h
@@ -17,21 +17,76 @@
 
 namespace sims
 {
+	class LockedVertexBuffer
+	{
+	public:
+		LockedVertexBuffer();
+		LockedVertexBuffer(VertexBuffer* vertexBuffer, uint32 lockFlags, uint32 offset, uint32 count);
+		
+		uint32 GetLockFlags() const { return lockFlags_; }
+		uint32 GetOffset() const { return offset_; }
+		uint32 GetCount() const { return count_; }
+
+		const VertexDeclarationRef& GetVertexDeclaration() const;
+		void* GetData();
+		const void* GetData() const;
+
+		void Init(VertexBuffer* vertexBuffer, uint32 lockFlags, uint32 offset, uint32 count);
+		void Clear();
+	private:
+		friend class VertexBuffer;
+
+		VertexBuffer* vertexBuffer_;
+		uint32 offset_;
+		uint32 count_;
+		uint32 lockFlags_;
+		Buffer* vertexData_;
+	};
+
 	class VertexBuffer
 	{
 	public:
+		enum VertexBufferLockFlags
+		{
+			LockRead = BIT(0),
+			LockWrite = BIT(1),
+			LockRW = LockRead | LockWrite
+		};
+
 		VertexBuffer();
 		VertexBuffer(const VertexDeclarationRef& vertexDecl, uint32 vertexCount);
+		virtual ~VertexBuffer();
 
-		const VertexDeclarationRef& GetVertexDeclaration() const;
-		int GetVertexCount() const;
+		bool Valid() const { return vertexData_.GetData() != nullptr; }
+		const VertexDeclarationRef& GetVertexDeclaration() const { return vertexDecl_; }
+		int GetVertexCount() const { return vertexCount_; }
 
 		void SetStorageFlags(uint32 flags) { storageFlags_ = flags; }
 		uint32 GetStorageFlags() const { return storageFlags_; }
+
+		// count 0 for lock total buffer
+		LockedVertexBuffer* Lock(uint32 lockFlags, uint32 offset = 0, uint32 count = 0);
+		void Unlock(LockedVertexBuffer* L);
+
+		// propagates changes on the vertex buffer to the renderer.
+		//   you must call invalidate after modifying vertex buffer data,
+		//   for it to be uploaded to GPU.
+		void Invalidate();
+
+		virtual void HWUpdateVertexBuffer() {}
+		virtual void HWBindVertexBuffer() {}
+		virtual void HWDeleteVertexBuffer() {}
 	private:
+		friend class LockedVertexBuffer;
+		Buffer* GetVertexData() { return &vertexData_; }
+	protected:
+		VertexDeclarationRef vertexDecl_;
+		uint32 vertexCount_;
+		Buffer vertexData_;
 		uint32 storageFlags_;
 
-		bool isLocked_;
+		LockedVertexBuffer lockedVB_;
+		int32 lockedCount_;
 	};
 }
 
